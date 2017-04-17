4444c510a217c04efc3a7a89292817999ad29780
Davide Pedranz <davide.pedranz@gmail.com>
2017-04-17 18:40:19 +0200

    Vulnerability #30

--- code/original/mysql/03_setup.sql
+++ code/original/mysql/03_setup.sql
@@ -5,6 +5,11 @@ USE schoolmate;
 INSERT INTO `users` (`userid`, `username`, `password`, `type`) VALUES (2, 'teacher', md5('teacher'), 'Teacher');
 INSERT INTO `teachers` (`teacherid`, `userid`, `fname`, lname) VALUES (1, 2, 'teacher', 'teacher');
 
+-- substitute
+INSERT INTO `users` (`userid`, `username`, `password`, `type`)
+VALUES (5, 'substitute', md5('substitute'), 'Substitute');
+INSERT INTO `teachers` (`teacherid`, `userid`, `fname`, lname) VALUES (2, 5, 'substitute', 'substitute');
+
 -- terms
 INSERT INTO `terms` (`termid`, `title`, `startdate`, `enddate`) VALUES (1, 'term', '2016-01-01', '2017-01-01');
 
@@ -13,10 +18,10 @@ INSERT INTO `semesters` (`semesterid`, `termid`, `title`, `startdate`, `midtermd
 VALUES (1, 1, 'semester', '2016-01-01', '2016-08-01', '2017-01-01');
 
 -- courses
-INSERT INTO `courses` (`courseid`, `semesterid`, `termid`, `coursename`, `teacherid`, `sectionnum`, `roomnum`,
-                       `periodnum`, `q1points`, `q2points`, `totalpoints`, `aperc`, `bperc`, `cperc`, `dperc`, `fperc`)
-VALUES (1, 1, 1, 'course', 1, 'section', 'room',
-           'ppp', 2.3, 4.5, 55.3, 1, 1, 1, 1, 1);
+INSERT INTO `courses` (`courseid`, `semesterid`, `termid`, `coursename`, `teacherid`, `sectionnum`, `roomnum`, `periodnum`,
+                       `q1points`, `q2points`, `totalpoints`, `aperc`, `bperc`, `cperc`, `dperc`, `fperc`, `substituteid`)
+VALUES (1, 1, 1, 'course', 1, 'section', 'room', 'ppp',
+           2.3, 4.5, 55.3, 1, 1, 1, 1, 1, 2);
 
 -- students
 INSERT INTO `users` (`userid`, `username`, `password`, `type`) VALUES (3, 'student', md5('student'), 'Student');
@@ -26,3 +31,7 @@ INSERT INTO `students` (`studentid`, `userid`, `fname`, `mi`, `lname`) VALUES (1
 INSERT INTO `users` (`userid`, `username`, `password`, `type`) VALUES (4, 'parent', md5('parent'), 'Parent');
 INSERT INTO `parents` (`parentid`, `userid`, `fname`, `lname`) VALUES (1, 4, 'parent', 'parent');
 INSERT INTO `parent_student_match` (`matchid`, `parentid`, `studentid`) VALUES (1, 1, 1);
+
+-- registrations
+INSERT INTO `registrations` (`regid`, `courseid`, `studentid`, `semesterid`, `termid`, `q1currpoints`, `q2currpoints`, `currentpoints`)
+VALUES (1, 1, 1, 1, 1, 1.2, 2.3, 3.2);
