718793bb7120371f11b7e07006cb08a32810ab72
Davide Pedranz <davide.pedranz@gmail.com>
2017-04-17 09:45:55 +0200

    Java project for unit tests - attack proof of concept

---
+++ code/tests/src/main/java/tests/utilities/Helper.java
@@ -0,0 +1,40 @@
+package tests.utilities;
+
+import net.sourceforge.jwebunit.junit.WebTester;
+
+/**
+ * Helper class to perform common operations in the testing of SchoolMate,
+ * for example the login operation.
+ *
+ * @author Davide Pedranz
+ */
+public final class Helper {
+
+    // constants
+    public static final String BASE_URL = "http://localhost:8000/";
+
+    // dependencies
+    private final WebTester tester;
+
+    /**
+     * Construct a new instance of the Helper.
+     *
+     * @param tester An instance of WebTester pointing to the vulnerable application.
+     */
+    public Helper(WebTester tester) {
+        this.tester = tester;
+    }
+
+    /**
+     * Perform a login from the home page.
+     *
+     * @param username Username.
+     * @param password Password.
+     */
+    public void login(String username, String password) {
+        tester.beginAt("index.php");
+        tester.setTextField("username", username);
+        tester.setTextField("password", password);
+        tester.submit();
+    }
+}
