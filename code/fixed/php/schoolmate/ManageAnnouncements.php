a165567b36e85c3b4ee40a452a29f6e421cdda21
Davide Pedranz <davide.pedranz@gmail.com>
2017-04-30 22:32:30 +0200

    Vulnerability #257

--- code/fixed/php/schoolmate/ManageAnnouncements.php
+++ code/fixed/php/schoolmate/ManageAnnouncements.php
@@ -158,11 +158,14 @@
    }
   }
 
+// sanitize!
+$onpage = htmlspecialchars($_POST[onpage]);
+
 print("\n</center>
   <input type='hidden' name='deleteannouncement'>
   <input type='hidden' name='selectannouncement'>
   <input type='hidden' name='page2' value='$page2'>
-  <input type='hidden' name='onpage' value='$_POST[onpage]'>
+  <input type='hidden' name='onpage' value='$onpage'>
   <input type='hidden' name='logout'>
   <input type='hidden' name='page' value='$page'>
  </form>
