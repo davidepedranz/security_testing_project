c2fca7188aa73d5b99c9895cf5d62ebcbb685f5c
Davide Pedranz <davide.pedranz@gmail.com>
2017-05-01 10:37:03 +0200

    Vulnerability #273

--- code/fixed/php/schoolmate/ManageTeachers.php
+++ code/fixed/php/schoolmate/ManageTeachers.php
@@ -178,11 +178,14 @@
    }
   }
 
+// sanitize!
+$onpage = htmlspecialchars($_POST[onpage]);
+
 print("\n</center>
   <input type='hidden' name='deleteteacher'>
   <input type='hidden' name='selectteacher'>
   <input type='hidden' name='page2' value='$page2'>
-  <input type='hidden' name='onpage' value='$_POST[onpage]'>
+  <input type='hidden' name='onpage' value='$onpage'>
   <input type='hidden' name='logout'>
   <input type='hidden' name='page' value='$page'>
  </form>
